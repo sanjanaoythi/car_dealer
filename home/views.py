c313d34d268daa77b5df86ef566add4ef0b04ab0
Fayaj Nakib <nakibfayaj99@gmail.com>
2022-12-12 06:12:18 +0600

    a little bit formatting everywhere

--- home/views.py
+++ home/views.py
@@ -13,33 +13,17 @@ def registrationPage(request):
     form = CreateUserForm()
     
     if request.method == 'POST':
+        form= CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
             user = form.cleaned_data.get('username')
-            messages.success(request,'Accout created for ' + user)
+            messages.success(request,'Account created for ' + user)
             return redirect('login')
 
      
     context ={'form':form}
     return render(request, 'registration.html', context)
     
-def loginpage(request):
-
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.info(request,'Username or password is incorrect' )
-    
-    return render(request,'login.html')
-
-
 def login_view(request):
 
     if request.method == 'POST':
@@ -55,6 +39,11 @@ def login_view(request):
             messages.info(request,'Username or password is incorrect' )
     
     return render(request,'login.html')
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
 
 
 
