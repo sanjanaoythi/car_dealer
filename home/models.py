f210f0ce22e7503b8b2feb90aa98a098968c25ae
Fayaj-Nakib <69723780+Fayaj-Nakib@users.noreply.github.com>
2023-09-17 10:27:10 +0600

    updated database

--- home/models.py
+++ home/models.py
@@ -23,3 +23,30 @@ class order(models.Model):
     Date_time = models.DateTimeField()
     Status = models.CharField(max_length=30)
     
+class rentcars(models.Model):
+    Car_ID = models.IntegerField(primary_key = True)
+    Type = models.CharField(max_length=30)
+    Brand = models.CharField(max_length=30)
+    Status = models.CharField(max_length=30)
+    Model = models.CharField(max_length=30)
+    Engine = models.CharField(max_length=30)
+    picture = models.ImageField()
+    fare = models.CharField(max_length=20)
+
+
+class rental(models.Model):
+    R_ID = models.IntegerField(primary_key = True)
+    Car_ID = models.ForeignKey(cars, default='1', on_delete=models.CASCADE)
+    id = models.ForeignKey(User, default='1', on_delete=models.CASCADE)
+    Date_time = models.DateTimeField()
+    Rent_hour = models.CharField(max_length=30)
+    Rent_fare = models.CharField(max_length=30)
+
+class payment(models.Model):
+    P_ID = models.IntegerField(primary_key = True)
+    Car_ID = models.ForeignKey(cars, default='1', on_delete=models.CASCADE)
+    id = models.ForeignKey(User, default='1', on_delete=models.CASCADE)
+    transaction_id=models.CharField(max_length=30)
+    transaction_amount=models.CharField(max_length=30)
+    Date_time = models.DateTimeField()
+    Status = models.CharField(max_length=30)
