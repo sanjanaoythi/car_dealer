406a76efdacc2c132346b217916ad682a18598c8
sanjanaoythi <84800071+sanjanaoythi@users.noreply.github.com>
2023-10-15 05:48:34 +0600

    Update models.py

--- home/models.py
+++ home/models.py
@@ -54,12 +54,31 @@ class order(models.Model):
     Status = models.CharField(max_length=30)
     
 class rentcars(models.Model):
+    TYPE_CHOICES = (
+        ('Sedan', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('Sports', 'Sports'),
+        ('Convertible', 'Convertible'),
+    )
+    BRAND_CHOICES = (
+        ('Audi', 'Audi'),
+        ('BMW', 'BMW'),
+        ('Honda', 'Honda'),
+        ('Land-Rover', 'Land-Rover'),
+        ('Mercedes', 'Mercedes'),
+        ('Toyota', 'Toyota'),
+    )
+    ENGINE_CHOICES = (
+        ('Petrol', 'Petrol'),
+        ('Diesel', 'Diesel'),
+    )
+
+
     Car_ID = models.IntegerField(primary_key = True)
-    Type = models.CharField(max_length=30)
-    Brand = models.CharField(max_length=30)
-    Status = models.CharField(max_length=30)
+    Type = models.CharField(max_length=30, choices=TYPE_CHOICES)
+    Brand = models.CharField(max_length=30, choices=BRAND_CHOICES)
     Model = models.CharField(max_length=30)
-    Engine = models.CharField(max_length=30)
+    Engine = models.CharField(max_length=30, choices=ENGINE_CHOICES)
     picture = models.ImageField()
     fare = models.CharField(max_length=20)
 
